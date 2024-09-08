fa7a20181608bf6b49d89569921a3318a117f662
Tharun <tharunv@Tharuns-MacBook-Air.local>
2024-09-08 23:06:45 +0530

    Reload error fixed

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { HashRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router basename={"/MyWebsite"}>
+  <Router basename="/">
     <App />
   </Router>
 );
